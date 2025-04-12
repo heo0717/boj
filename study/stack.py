040a302417f6a7fb1763929dd94e17e819e300c7
Yeon <heo0717@naver.com>
2025-04-12 22:55:51 +0900

    solved

--- study/stack.py
+++ study/stack.py
@@ -3,28 +3,28 @@ input = sys.stdin.readline
 
 n = int(input().strip())
 
-queue = []
+stack = []
 i = 0
 while i < n:
     order = input().strip().split()
 
     if order[0] == 'push':
-        queue.append(int(order[1]))
+        stack.append(int(order[1]))
 
     elif order[0] == 'pop':
-        if not queue :
+        if not stack :
             print('-1')
         else:
-            p = queue.pop(-1)
+            p = stack.pop(-1)
             print(p)
 
     elif order[0] == 'size':
-        print(len(queue))
+        print(len(stack))
 
     elif order[0] == 'empty':
-        print( 1 if not queue else 0)
+        print( 1 if not stack else 0)
 
     elif order[0] == 'top':
-        print( -1 if not queue else queue[-1])
+        print( -1 if not stack else stack[-1])
 
     i += 1 
