f9b78c32fbffae870265bcc7b9a1863ba162d798
YeonSeungHee <heo0717@naver.com>
2025-07-16 23:14:13 +0900

    solved

--- study/part_timer_gangho.py
+++ study/part_timer_gangho.py
@@ -8,12 +8,6 @@ T = sorted([ int(input().strip()) for _ in range(N) ], reverse = True)
 tip = 0
 
 for i in range(N):
-
-    t = T[i] - i
-
-    print(t)
-
-    if t > 0 : tip += t
-
-print(T)
+     t = T[i] - i
+     if t > 0 : tip += t
 print(tip)
